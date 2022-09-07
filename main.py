14723976d082fb96a75b84e73a12b4d6dab2fa09
HY_LEE <2gnldud@gmail.com>
2022-09-07 12:13:44 +0900

    test_commit

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-print("Hello_World")
-print("Github")
+from fastapi import FastAPI
+app = FastAPI()
+
+from fastapi.responses import FileResponse
+
+@app.get("/")
+def hello():
+    return FileResponse('index.html')
+
+from pydantic import BaseModel
+class Model(BaseModel):
+    name : str
+    phone : int
+
+@app.post("/send")
+def send(data : Model):
+    print(data)
+    return "전송완료"
+
